e3b3448c4722741427aa42c505c6bb6e4fd7a378
dipsy <dipsik@gmail.com>
2014-08-18 18:56:12 +0400

    Update SimpleThingsEntityAuditExtension.php
    added missed 'global_ignore_columns' (global_ignore_columns config not work)

--- src/SimpleThings/EntityAudit/DependencyInjection/SimpleThingsEntityAuditExtension.php
+++ src/SimpleThings/EntityAudit/DependencyInjection/SimpleThingsEntityAuditExtension.php
@@ -47,7 +47,8 @@ class SimpleThingsEntityAuditExtension extends Extension
             'revision_field_name',
             'revision_type_field_name',
             'revision_table_name',
-            'revision_id_field_type'
+            'revision_id_field_type',
+            'global_ignore_columns'
         );
 
         foreach ($configurables as $key) {
