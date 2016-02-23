ab8f3636f8aa6bbe2d68c748e2fc8fe9a479158c
David Edwards <david.edwards@oneresult.co.uk>
2016-02-23 20:31:30 +0000

    Regression test for issue #156

---
+++ tests/SimpleThings/Tests/EntityAudit/Fixtures/Issue/Issue156Contact.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 23/02/2016
+ * Time: 15:57
+ */
+
+namespace SimpleThings\EntityAudit\Tests\Fixtures\Issue;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Issue156Contact
+ * @package SimpleThings\EntityAudit\Tests\Fixtures\Issue
+ * @ORM\Entity()
+ * @ORM\InheritanceType("SINGLE_TABLE")
+ * @ORM\DiscriminatorColumn(name="discriminator", type="string")
+ */
+class Issue156Contact
+{
+    /** @var int @ORM\Id @ORM\Column(type="integer") @ORM\GeneratedValue(strategy="AUTO") */
+    protected $id;
+
+    /**
+     * @var ArrayCollection|Issue156ContactTelephoneNumber[]
+     * ORM\OneToMany(targetEntity="Issue156ContactTelephoneNumber", mappedBy="contact")
+     */
+    protected $telephoneNumbers;
+
+    public function __construct()
+    {
+        $this->telephoneNumbers = new ArrayCollection();
+    }
+
+    /**
+     * @param int $id
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param Issue156ContactTelephoneNumber $telephoneNumber
+     * @return $this
+     */
+    public function addTelephoneNumber(Issue156ContactTelephoneNumber $telephoneNumber)
+    {
+        if (!$this->telephoneNumbers->contains($telephoneNumber)) {
+            $telephoneNumber->setContact($this);
+            $this->telephoneNumbers[] = $telephoneNumber;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param Issue156ContactTelephoneNumber $telephoneNumber
+     * @return $this
+     */
+    public function removeTelephoneNumber(Issue156ContactTelephoneNumber $telephoneNumber)
+    {
+        $this->telephoneNumbers->removeElement($telephoneNumber);
+
+        return $this;
+    }
+
+    /**
+     * @return ArrayCollection|Issue156ContactTelephoneNumber[]
+     */
+    public function getTelephoneNumbers()
+    {
+        return $this->telephoneNumbers;
+    }
+}
---
+++ tests/SimpleThings/Tests/EntityAudit/Fixtures/Issue/Issue156ContactTelephoneNumber.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 23/02/2016
+ * Time: 15:57
+ */
+
+namespace SimpleThings\EntityAudit\Tests\Fixtures\Issue;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Issue156ContactTelephoneNumber
+ * @package SimpleThings\EntityAudit\Tests\Fixtures\Issue
+ * @ORM\Entity()
+ */
+class Issue156ContactTelephoneNumber
+{
+    /** @ORM\Id @ORM\Column(type="integer") @ORM\GeneratedValue(strategy="AUTO") */
+    protected $id;
+
+    /**
+     * @var Issue156Contact
+     * @ORM\ManyToOne(targetEntity="Issue156Contact", inversedBy="telephoneNumbers")
+     */
+    private $contact;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $number;
+
+    /**
+     * @param mixed $id
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param Issue156Contact $contact
+     * @return $this
+     */
+    public function setContact(Issue156Contact $contact = null)
+    {
+        $this->contact = $contact;
+
+        return $this;
+    }
+
+    /**
+     * @return Issue156Contact
+     */
+    public function getContact()
+    {
+        return $this->contact;
+    }
+
+    /**
+     * @param string $number
+     * @return $this
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+}
