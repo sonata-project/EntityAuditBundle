9aa6a6025d274b136485baeeade4bec46e2b4b86
Tseho <quentin.favrie@gmail.com>
2016-09-14 11:33:21 +0200

    Removed unused namespace, added comments

--- src/SimpleThings/EntityAudit/AuditConfigurationFactory.php
+++ src/SimpleThings/EntityAudit/AuditConfigurationFactory.php
@@ -3,13 +3,20 @@
 namespace SimpleThings\EntityAudit;
 
 use Doctrine\Common\Annotations\Reader;
-use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\ORM\Configuration;
 
 class AuditConfigurationFactory
 {
     const ANNOTATION_AUDITABLE = 'SimpleThings\\EntityAudit\\Mapping\\Annotation\\Auditable';
 
+    /**
+     * This is the factory method used by the Symfony DependencyInjection
+     *
+     * @param Configuration $doctrineConfiguration
+     * @param Reader $reader
+     * @param array $auditedEntities
+     * @return AuditConfiguration
+     */
     public function createAuditConfiguration(Configuration $doctrineConfiguration, Reader $reader, $auditedEntities = array())
     {
         $entities = $doctrineConfiguration
