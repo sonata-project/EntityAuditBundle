dcd983431cb622912374f24353d4cd81b6dbfaea
Gonzalo Alonso <gonkpo@gmail.com>
2017-08-31 13:46:00 -0300

    Fix columnName
    $columnName = $idKeys[0]; change by $columnName = $idField;

--- src/SimpleThings/EntityAudit/AuditReader.php
+++ src/SimpleThings/EntityAudit/AuditReader.php
@@ -234,7 +234,7 @@ class AuditReader
         foreach ($class->identifier AS $idField) {
             if (is_array($id) && count($id) > 0) {
                 $idKeys = array_keys($id);
-                $columnName = $idKeys[0];
+                $columnName = $idField;
             } else if (isset($class->fieldMappings[$idField])) {
                 $columnName = $class->fieldMappings[$idField]['columnName'];
             } elseif (isset($class->associationMappings[$idField])) {
