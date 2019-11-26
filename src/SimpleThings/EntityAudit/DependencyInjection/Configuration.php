9e01ce1e438543a85c1ad8086d9d83736ef302e7
Aleksandar Dimitrov <netbull2007@gmail.com>
2019-11-26 10:05:35 +0200

    Compatibility with Symfony 5

--- src/SimpleThings/EntityAudit/DependencyInjection/Configuration.php
+++ src/SimpleThings/EntityAudit/DependencyInjection/Configuration.php
@@ -9,8 +9,8 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $builder = new TreeBuilder();
-        $builder->root('simple_things_entity_audit')
+        $builder = new TreeBuilder('simple_things_entity_audit');
+        $builder->getRootNode()
             ->children()
                 ->scalarNode('entity_manager')->defaultValue('default')->end()
                 ->scalarNode('table_prefix')->defaultValue('')->end()
