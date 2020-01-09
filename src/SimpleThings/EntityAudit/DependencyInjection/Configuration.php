005c278062b189585b01aabb56991652a6fc0fe8
vlad <vlad@pmconnect.co.uk>
2020-01-09 13:20:14 +0000

    symfony 4.3+ compatibility

--- src/SimpleThings/EntityAudit/DependencyInjection/Configuration.php
+++ src/SimpleThings/EntityAudit/DependencyInjection/Configuration.php
@@ -9,8 +9,17 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $builder = new TreeBuilder();
-        $builder->root('simple_things_entity_audit')
+        $builder = new TreeBuilder('simple_things_entity_audit');
+
+        if (\method_exists($builder, 'getRootNode')) {
+            // SF 4.3+ compatibility
+            $rootNode = $builder->getRootNode();
+        } else {
+            // Older SF
+            $rootNode = $builder->root('simple_things_entity_audit');
+        }
+
+        $rootNode
             ->children()
                 ->arrayNode('audited_entities')
                     ->prototype('scalar')->end()
