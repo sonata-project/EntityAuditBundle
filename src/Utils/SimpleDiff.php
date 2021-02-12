4f221c20da1bf0faedf6a73aae88acb5cbc4c1fb
Vincent Langlet <vincentlanglet@hotmail.fr>
2021-02-12 18:48:53 +0100

    Lint header

--- src/Utils/SimpleDiff.php
+++ src/Utils/SimpleDiff.php
@@ -3,22 +3,13 @@
 declare(strict_types=1);
 
 /*
-    Paul's Simple Diff Algorithm v 0.1
-    (C) Paul Butler 2007 <http://www.paulbutler.org/>
-    May be used and distributed under the zlib/libpng license.
-
-    This code is intended for learning purposes; it was written with short
-    code taking priority over performance. It could be used in a practical
-    application, but there are a few ways it could be optimized.
-
-    Given two arrays, the function diff will return an array of the changes.
-    I won't describe the format of the array, but it will be obvious
-    if you use print_r() on the result of a diff on some test data.
-
-    htmlDiff is a wrapper for the diff command, it takes two strings and
-    returns the differences in HTML. The tags used are <ins> and <del>,
-    which can easily be styled with CSS.
-*/
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace SimpleThings\EntityAudit\Utils;
 
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 if (!($loader = @include __DIR__.'/../vendor/autoload.php')) {
     die(<<<'EOT'
 You must set up the project dependencies, run the following commands:
