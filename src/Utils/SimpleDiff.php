9bd09023e1a150b05246c91d257373671cd66bd0
Micheal <micheal.mouner@gmail.com>
2021-07-26 13:31:53 +0200

    Remove unused vars

--- src/Utils/SimpleDiff.php
+++ src/Utils/SimpleDiff.php
@@ -50,8 +50,6 @@ class SimpleDiff
     {
         $ret = '';
         $diff = $this->diff(explode(' ', $old), explode(' ', $new));
-        $nmax = 0;
-        $omax = 0;
         foreach ($diff as $k) {
             if (\is_array($k)) {
                 $ret .= (!empty($k['d']) ? '<del>'.implode(' ', $k['d']).'</del> ' : '').
