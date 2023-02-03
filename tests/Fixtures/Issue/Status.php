071b59a4fecc8cc94a22501c6244d45c32a4a02b
Antonio Pauletich <antonio.pauletich95@gmail.com>
2023-02-03 18:43:50 +0100

    Fix audit revision query for enum column mapping

---
+++ tests/Fixtures/Issue/Status.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sonata\EntityAuditBundle\Tests\Fixtures\Issue;
+
+enum Status: string
+{
+    case Foo = 'bar';
+    case Qwe = 'baz';
+}
---
+++ tests/Issue/IssueEntityWithEnumTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\EntityAuditBundle\Tests\Issue;
+
+use Sonata\EntityAuditBundle\Tests\BaseTest;
+use Sonata\EntityAuditBundle\Tests\Fixtures\Issue\IssueEntityWithEnum;
+use Sonata\EntityAuditBundle\Tests\Fixtures\Issue\Status;
+
+/**
+ * @requires PHP 8.1
+ */
+final class IssueEntityWithEnumTest extends BaseTest
+{
+    protected $schemaEntities = [
+        IssueEntityWithEnum::class,
+    ];
+
+    protected $auditedEntities = [
+        IssueEntityWithEnum::class,
+    ];
+
+    public function testIssueEntityWithEnums(): void
+    {
+        $entity = new IssueEntityWithEnum(Status::Foo);
+
+        $this->em->persist($entity);
+        $this->em->flush();
+
+        \assert(\is_int($entity->getId()));
+
+        $this->em->clear();
+
+        /** @var IssueEntityWithEnum $entity */
+        $entity = $this->em->getRepository(IssueEntityWithEnum::class)->findOneBy(['id' => $entity->getId()]);
+
+        $entity->setStatus(Status::Qwe);
+        $this->em->flush();
+
+        $reader = $this->auditManager->createAuditReader($this->em);
+
+        $auditEntity = $reader->find(IssueEntityWithEnum::class, $entity->getId(), 1);
+        self::assertInstanceOf(IssueEntityWithEnum::class, $auditEntity);
+        self::assertSame(Status::Foo, $auditEntity->getStatus());
+
+        $auditEntity = $reader->find(IssueEntityWithEnum::class, $entity->getId(), 2);
+        self::assertInstanceOf(IssueEntityWithEnum::class, $auditEntity);
+        self::assertSame(Status::Qwe, $auditEntity->getStatus());
+
+        $this->em->clear();
+
+        /** @var IssueEntityWithEnum $entity */
+        $entity = $this->em->getRepository(IssueEntityWithEnum::class)->findOneBy(['id' => $entity->getId()]);
+
+        $this->em->remove($entity);
+        $this->em->flush();
+
+        self::assertCount(0, $reader->findRevisions(IssueEntityWithEnum::class, $entity->getId()));
+    }
+}
