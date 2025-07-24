e6dbc72e529fe959b887e1648666f2abefb93b01
Valentin Karnauhov <v.karnauhov@itteco.lv>
2025-07-24 18:15:12 +0300

    Fix for non-existing revision in many-to-many association

--- src/Collection/AuditedCollection.php
+++ src/Collection/AuditedCollection.php
@@ -323,9 +323,10 @@ class AuditedCollection implements Collection
     }
 
     /**
+     * @phpstan-template U
+     *
      * @return Collection<TKey, U>
      *
-     * @phpstan-template U
      * @phpstan-param \Closure(T):U $func
      * @phpstan-return Collection<TKey, U>
      */
