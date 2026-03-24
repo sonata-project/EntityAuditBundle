0b9349f42fbe9929188b287455edb00e79d76b56
Valentin Karnauhov <v.karnauhov@itteco.lv>
2026-03-24 16:06:30 +0200

    Fix unnecessary revision creation when only a column from the globalIgnoreColumns list is updated

--- src/EventListener/LogRevisionsListener.php
+++ src/EventListener/LogRevisionsListener.php
@@ -724,6 +724,10 @@ class LogRevisionsListener implements EventSubscriber
                 continue;
             }
 
+            if (\in_array($field, $this->config->getGlobalIgnoreColumns(), true)) {
+                continue;
+            }
+
             $newVal = $change[1];
 
             if (!isset($classMetadata->associationMappings[$field])) {
